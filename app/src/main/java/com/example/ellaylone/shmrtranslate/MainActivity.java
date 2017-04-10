9cedcd6c9d8efbc12f4de2ac2028f663f1edbf8d
Ellaylone <ellaylone@yandex-team.ru>
2017-04-10 21:54:28 +0300

    trying action bar

--- app/src/main/java/com/example/ellaylone/shmrtranslate/MainActivity.java
+++ app/src/main/java/com/example/ellaylone/shmrtranslate/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ellaylone.shmrtranslate;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // создаем кастомный тулбар
+        Toolbar customToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        customToolbar.setTitle("");
+        setSupportActionBar(customToolbar);
+
+        // создаем спиннер
+        populateSpinner(R.id.origin_language);
+        populateSpinner(R.id.translation_language);
+
+        // кастомный ViewPager чтобы избавиться от перелистывания фрагментов свайпом
         SwipeViewPager swipeViewPager = (SwipeViewPager) findViewById(R.id.viewpager);
 
         CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
@@ -21,5 +34,13 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         tabLayout.setupWithViewPager(swipeViewPager);
+    }
+
+    private void populateSpinner (int spinnerResourceId) {
+        Spinner spinner = (Spinner) findViewById(spinnerResourceId);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.languages_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
