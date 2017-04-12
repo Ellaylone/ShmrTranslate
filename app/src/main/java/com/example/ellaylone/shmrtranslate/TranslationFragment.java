13ecd136a71456411c3f59d0bc8ae1db4a7f36f3
Ellaylone <ellaylone@yandex-team.ru>
2017-04-12 21:02:29 +0300

    text area focus

--- app/src/main/java/com/example/ellaylone/shmrtranslate/TranslationFragment.java
+++ app/src/main/java/com/example/ellaylone/shmrtranslate/TranslationFragment.java
@@ -2,13 +2,13 @@ package com.example.ellaylone.shmrtranslate;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.Toast;
 
 /**
  * Created by ellaylone on 09.04.17.
@@ -20,20 +20,30 @@ public class TranslationFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_translation, container, false);
 
         EditText textSource = (EditText) view.findViewById(R.id.text_area_source);
 
+        final TabLayout tabLayout = (TabLayout) getActivity().findViewById(R.id.tabs);
+
         textSource.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
-
-                Toast.makeText(getActivity(), "Has focus: " + hasFocus, Toast.LENGTH_SHORT).show();
+                if (!hasFocus) {
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
+                    new android.os.Handler().postDelayed(
+                        new Runnable() {
+                            public void run() {
+                                tabLayout.setVisibility(View.VISIBLE);
+                            }
+                        }, 100);
+                } else {
+                    tabLayout.setVisibility(View.GONE);
+                }
             }
         });
 
